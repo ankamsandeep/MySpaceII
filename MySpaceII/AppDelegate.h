9d0111a7d2c95cc7bebabccbb6b38f546060b884
Sandeep Ankam <sandeepankam@me.com>
2015-09-01 21:22:19 -0700

    Created single view Xcode project.

---
+++ MySpaceII/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySpaceII
+//
+//  Created by Sandeep Ankam on 9/1/15.
+//  Copyright (c) 2015 Sandeep Ankam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySpaceII/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MySpaceII
+//
+//  Created by Sandeep Ankam on 9/1/15.
+//  Copyright (c) 2015 Sandeep Ankam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
